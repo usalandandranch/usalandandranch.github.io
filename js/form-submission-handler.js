22f65ecbcddaf9dcb67209584d443353ace3a818
usalandandranch <usalandandranch@gmail.com>
2018-07-03 11:46:26 -0500

    Version 3

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -122,4 +122,18 @@ function loaded() {
   }, false);
     // form.addEventListener("submit", handleFormSubmit, false);
 };
+function displayForm(form_ID, thankyou_ID) {
+  // display form and hide thankyou message
+  document.getElementById(thankyou_ID).style.display="none";
+  document.getElementById(form_ID).style.display = "block";
+  document.getElementById("message").value="";
+}
+
+function fillForm(message)  {
+  // fill form with message
+  document.getElementById("message").value="I am interested in "+message+".";
+  // $('.modal').modal('hide');
+  document.getElementById("contact-in-menu").click();
+}
+
 document.addEventListener("DOMContentLoaded", loaded, false);
